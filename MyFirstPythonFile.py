9db19ff88bf8ceba094b2f11db3d95a127116899
Gyanesh Soni <48016555+gyanusoni@users.noreply.github.com>
2024-10-29 19:25:03 +0530

    My first python file

---
+++ MyFirstPythonFile.py
@@ -0,0 +1,16 @@
+#print("Hello World!")
+
+#Name = input("What is your name? ")
+
+#print("Hello " + Name)
+
+#birth_year = input("Please enter your birth year ")
+
+#print("You were born in " + birth_year)
+#age = 2024 - int(birth_year)
+#print("Your age is  " + str(age))
+
+course = 'Python for Beginners'
+print(course.upper())
+print(course.find('o'))
+print(course.replace('for', '4'))
